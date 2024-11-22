fdbad6b23ba574c927b2228c2b25e20eb66cf290
Kumbirai Nenguke <ST10383430@VCCONNECT.EDU.ZA>
2024-11-22 23:18:58 +0200

    Dbcontext established

--- PORG6212_Part3 version 3/ApplicationDbContext.cs
+++ PORG6212_Part3 version 3/ApplicationDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PORG6212_Part3_version_3.Models;
 
 namespace PORG6212_Part3_version_3
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<LecturerClaim> LecturerClaims { get; set; }
     }
 }
