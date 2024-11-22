4db35ccc889a06387318a6b4db504dd54b3aeadd
Kumbirai Nenguke <ST10383430@VCCONNECT.EDU.ZA>
2024-11-22 23:30:02 +0200

    HR Controller established

--- PORG6212_Part3 version 3/Controllers/HRController.cs
+++ PORG6212_Part3 version 3/Controllers/HRController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PORG6212_Part3_version_3.Data;
 
 namespace PORG6212_Part3_version_3.Controllers
 {
     public class HRController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HRController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult FinalizeClaims()
+        {
+            var approvedClaims = _context.LecturerClaims.Where(c => c.Status == "Approved").ToList();
+            return View(approvedClaims);
         }
     }
 }
