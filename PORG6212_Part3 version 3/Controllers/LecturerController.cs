76d2ead49c59f21ddf637978ad27436d79a9657d
Kumbirai Nenguke <ST10383430@VCCONNECT.EDU.ZA>
2024-11-22 23:22:21 +0200

    LecturerControler established

---
+++ PORG6212_Part3 version 3/Controllers/LecturerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using PORG6212_Part3_version_3.Models;
+
+namespace PORG6212_Part3_version_3.Controllers
+{
+    public class LecturerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LecturerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitClaim(LecturerClaim claim)
+        {
+            if (ModelState.IsValid)
+            {
+                claim.TotalPayment = claim.HoursWorked * claim.HourlyRate;
+                _context.LecturerClaims.Add(claim);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Claim submitted successfully!";
+                return RedirectToAction(nameof(SubmitClaim));
+            }
+
+            return View(claim);
+        }
+    }
+}
