5f3cd75a86f5d323e9d889a81206659d83320a89
Kumbirai Nenguke <ST10383430@VCCONNECT.EDU.ZA>
2024-11-22 23:08:20 +0200

    Add project files.

---
+++ PORG6212_Part3 version 3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PORG6212_Part3_version_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
