95d5564efb898dff7c010b5d2f22faf2f2c1c86c
Kumbirai Nenguke <ST10383430@VCCONNECT.EDU.ZA>
2024-11-22 23:14:18 +0200

    Lecturer claims class established

---
+++ PORG6212_Part3 version 3/Models/LecturerClaim.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PORG6212_Part3_version_3.Models
+{
+    public class LecturerClaim
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Hours must be between 1 and 100.")]
+        public int HoursWorked { get; set; }
+
+        [Required]
+        [Range(10, 500, ErrorMessage = "Hourly Rate must be between 10 and 500.")]
+        public decimal HourlyRate { get; set; }
+
+        public decimal TotalPayment { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Pending";
+    }
+}
